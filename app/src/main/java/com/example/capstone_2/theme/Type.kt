dcfb2d13c0e23ac787ae813080a5062b979749f3
Byblos <mgim1547@gmail.com>
2025-08-01 14:15:18 +0900

    Update Type.kt

--- app/src/main/java/com/example/capstone_2/theme/Type.kt
+++ app/src/main/java/com/example/capstone_2/theme/Type.kt
@@ -2,33 +2,35 @@ package com.example.capstone_2.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.capstone_2.R
 
-// Set of Material typography styles to start with
+
+val NanumGothic = FontFamily(
+    Font(R.font.nanumgothic_regular, FontWeight.Normal),
+    Font(R.font.nanumgothic_bold, FontWeight.Bold),
+    Font(R.font.nanumgothic_extrabold, FontWeight.ExtraBold)
+)
+
+val NotoSansCondensed = FontFamily(
+    Font(R.font.notosans_condensed_medium, FontWeight.Medium),
+    Font(R.font.notosans_semicondensed_bold, FontWeight.Bold),
+    Font(R.font.notosans_semicondensed_thin, FontWeight.Thin)
+)
+
+// 전체 앱에 적용할 Typography 설정
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NanumGothic,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 16.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleLarge = TextStyle(
+        fontFamily = NanumGothic,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     )
-    */
 )
