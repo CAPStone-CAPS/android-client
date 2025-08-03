096c7559da0ca1cf1d0fb5a4c79ed3219f5c1858
박가령 <garyoung3749@naver.com>
2025-08-03 15:25:50 +0900

    PastHistoryScreen 추가 및 navigationBar 적용

--- app/src/main/java/com/example/capstone_2/MainActivity.kt
+++ app/src/main/java/com/example/capstone_2/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.capstone_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.capstone_2.ui.AppUsageTrackerScreen
+import com.example.capstone_2.ui.navigation.MainScreen
 import com.example.capstone_2.ui.theme.CapstoneTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CapstoneTheme {
-                AppUsageTrackerScreen(context = this)
+                MainScreen(context = this@MainActivity)
             }
         }
     }
